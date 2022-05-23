ea1ea6035b7a3568d5b1ff859cf6aff6e832e62f
Marius <Mnv22081@gmail.com>
2022-05-23 21:45:39 +0300

    Added Race Carousel

--- src/main/java/com/example/sefproject/Controller.java
+++ src/main/java/com/example/sefproject/Controller.java
@@ -1,7 +1,24 @@
 package com.example.sefproject;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToRace(ActionEvent event) throws IOException {
+        SceneSwitcher scene = new SceneSwitcher();
+        scene.switchScene("raceCar.fxml",event);
+    }
 }
---
+++ src/main/java/com/example/sefproject/raceCar.java
@@ -0,0 +1,93 @@
+package com.example.sefproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class raceCar {
+
+    private int current = 1;
+    private String[] races = new String[] {
+            "Dwarf","Elf","Halfling"
+            ,"Human","Dragonborn","Gnome",
+            "Half-Elf","Half-Orc","Tiefling" };
+
+    @FXML
+    private ImageView ImageLeft;
+    @FXML
+    private ImageView ImageCenter;
+    @FXML
+    private ImageView ImageRight;
+    @FXML
+    private Button LabelLeft;
+    @FXML
+    private Button LabelCenter;
+    @FXML
+    private Button LabelRight;
+    @FXML
+    private Button RightButton;
+    @FXML
+    private Button LeftButton;
+
+    public void rightArrow() {
+        current++;
+        switchCar(current);
+    }
+
+    public void leftArrow() {
+        current--;
+        switchCar(current);
+    }
+
+    public void switchCar(int now) {
+        if (now == 1) {
+
+            Image left = new Image(getClass().getResourceAsStream("Images/Dwarf.png"));
+            ImageLeft.setImage(left);
+            Image center = new Image(getClass().getResourceAsStream("Images/Elf.png"));
+            ImageCenter.setImage(center);
+            Image right = new Image(getClass().getResourceAsStream("Images/Halfling.png"));
+            ImageRight.setImage(right);
+
+            LabelLeft.setText(races[0]);
+            LabelCenter.setText(races[1]);
+            LabelRight.setText(races[2]);
+
+            LeftButton.setId("hidden");
+        }
+
+        if (now == 2) {
+            Image left = new Image(getClass().getResourceAsStream("Images/Human.png"));
+            ImageLeft.setImage(left);
+            Image center = new Image(getClass().getResourceAsStream("Images/Dragonborn.png"));
+            ImageCenter.setImage(center);
+            Image right = new Image(getClass().getResourceAsStream("Images/Gnome.png"));
+            ImageRight.setImage(right);
+
+            LabelLeft.setText(races[3]);
+            LabelCenter.setText(races[4]);
+            LabelRight.setText(races[5]);
+
+            RightButton.setId("shown");
+            LeftButton.setId("shown");
+        }
+
+        if (now == 3) {
+            Image left = new Image(getClass().getResourceAsStream("Images/Half-Elf.png"));
+            ImageLeft.setImage(left);
+            Image center = new Image(getClass().getResourceAsStream("Images/Half-Orc.png"));
+            ImageCenter.setImage(center);
+            Image right = new Image(getClass().getResourceAsStream("Images/Tiefling.png"));
+            ImageRight.setImage(right);
+
+            LabelLeft.setText(races[6]);
+            LabelCenter.setText(races[7]);
+            LabelRight.setText(races[8]);
+
+            RightButton.setId("hidden");
+        }
+
+    }
+
+}
