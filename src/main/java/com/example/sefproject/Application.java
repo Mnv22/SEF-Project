f6295f3af672f6de335d8ee990039f344510bf31
Sorin Talaba <t.sorin2009@gmail.com>
2022-05-24 17:13:58 +0300

    Added basic LogIn implementation

--- src/main/java/com/example/sefproject/Application.java
+++ src/main/java/com/example/sefproject/Application.java
@@ -9,8 +9,14 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
+
+    private static Stage stg;
+
     @Override
     public void start(Stage stage) throws IOException {
+
+        stg=stage;
+
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("login.fxml"));
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root, 1200, 800);
@@ -22,6 +28,12 @@ public class Application extends javafx.application.Application {
         stage.setScene(scene);
         stage.show();
         root.requestFocus();
+    }
+
+    public void changeScene(String fxml)throws IOException{
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+        stg.getScene().setRoot(pane);
+
     }
 //we will add some login features
     public static void main(String[] args) {
--- src/main/java/com/example/sefproject/login.java
+++ src/main/java/com/example/sefproject/login.java
@@ -1,13 +1,20 @@
 package com.example.sefproject;
 
-import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
 
 import java.io.IOException;
 
-public class login {
+
+/*public class login {
 
     private Stage stage;
     private Scene scene;
@@ -17,4 +24,50 @@ public class login {
         SceneSwitcher scene = new SceneSwitcher();
         scene.switchScene("raceCar.fxml",event);
     }
+}
+*/
+
+public class login {
+
+    public login(){
+
+    }
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private Label wrongLogIN;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
+
+
+    public void userLogIn(ActionEvent event) throws IOException{
+        checkLogIn();
+    }
+
+    private void checkLogIn()throws IOException{
+        Application n= new Application();
+        if(username.getText().toString().equals("nume")&&password.getText().toString().equals("parola")){
+            wrongLogIN.setText("Succes");
+
+            n.changeScene("raceCar.fxml");
+
+        }
+
+        else if(username.getText().isEmpty() && password.getText().isEmpty()){
+            wrongLogIN.setText("Please type your username and password");
+        }
+
+        else{
+            wrongLogIN.setText("Wrong username or password.");
+        }
+
+    }
+
+
 }
